20e46b5ad600f50c5d6c08b86604ffb109ad4de7
karenmillermoj <karen.miller@digital.justice.gov.uk>
2021-04-27 11:50:10 +0100

    DT-1786 Add security config

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,13 @@ configurations {
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springframework:spring-jms")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+  implementation("org.springframework:spring-jms")
+
   implementation(platform("com.amazonaws:aws-java-sdk-bom:1.11.1001"))
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
   implementation("org.apache.commons:commons-lang3:3.12.0")
@@ -21,6 +25,7 @@ dependencies {
   runtimeOnly("org.postgresql:postgresql:42.2.19")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
+  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
 }
 
 tasks {
