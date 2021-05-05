8adec6f5272e9140844bb00073a6eb4908b420d7
karenmillermoj <karen.miller@digital.justice.gov.uk>
2021-05-05 13:46:56 +0100

    DT-1970 Separate aws config for different environments

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/config/SqsConfig.kt
@@ -0,0 +1,42 @@
+package uk.gov.justice.digital.hmpps.hmppsauditapi.config
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.sqs.AmazonSQS
+import com.amazonaws.services.sqs.AmazonSQSAsync
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+
+@Configuration
+class SqsConfig {
+
+  @Bean
+  @Primary
+  @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "aws")
+  fun awsSqsClient(
+    @Value("\${sqs.aws.access.key.id}") accessKey: String,
+    @Value("\${sqs.aws.secret.access.key}") secretKey: String,
+    @Value("\${sqs.endpoint.region}") region: String
+  ): AmazonSQSAsync =
+    AmazonSQSAsyncClientBuilder.standard()
+      .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
+      .withRegion(region)
+      .build()
+
+  @Bean
+  @ConditionalOnProperty(name = ["sqs.provider"], havingValue = "aws")
+  fun awsSqsDlqClient(
+    @Value("\${sqs.aws.dlq.access.key.id}") accessKey: String,
+    @Value("\${sqs.aws.dlq.secret.access.key}") secretKey: String,
+    @Value("\${sqs.endpoint.region}") region: String
+  ): AmazonSQS =
+    AmazonSQSClientBuilder.standard()
+      .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
+      .withRegion(region)
+      .build()
+}
