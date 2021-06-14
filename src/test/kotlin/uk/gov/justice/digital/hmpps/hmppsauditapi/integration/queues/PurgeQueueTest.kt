4de355122310758c7a5316b5f79dd6debb1bce73
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-06-14 16:37:14 +0100

    DT-1971 Use the purge endopints from the queue library

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/integration/queues/PurgeQueueTest.kt
@@ -0,0 +1,69 @@
+package uk.gov.justice.digital.hmpps.hmppsauditapi.integration.queues
+
+import org.awaitility.kotlin.await
+import org.awaitility.kotlin.matches
+import org.awaitility.kotlin.untilCallTo
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.http.MediaType
+import uk.gov.justice.digital.hmpps.hmppsauditapi.resource.QueueListenerIntegrationTest
+
+class PurgeQueueTest : QueueListenerIntegrationTest() {
+
+  @Nested
+  inner class PurgeDlq {
+    @Test
+    fun `should fail if no token`() {
+      webTestClient.put()
+        .uri("/queue-admin/purge-queue/${sqsConfigProperties.dlqName}")
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
+
+    @Test
+    fun `should fail if wrong role`() {
+      webTestClient.put()
+        .uri("/queue-admin/purge-queue/${sqsConfigProperties.dlqName}")
+        .headers(setAuthorisation(roles = listOf("ROLE_WRONG")))
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `should fail it dlq does not exist`() {
+      webTestClient.put()
+        .uri("/queue-admin/purge-queue/UNKNOWN_DLQ")
+        .headers(setAuthorisation(roles = listOf("ROLE_QUEUE_ADMIN"), scopes = listOf("write")))
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isNotFound
+    }
+
+    @Test
+    fun `should purge message from the DLQ`() {
+      val message = """
+    {
+      "what": "OFFENDER_DELETED",
+      "when": "2021-01-25T12:30:00Z",
+      "operationId": "badea6d876c62e2f5264c94c7b50875e",
+      "who": "bobby.beans",
+      "service": "offender-service",
+      "details": "{ \"offenderId\": \"99\"}"
+    }
+  """
+      await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 0 }
+      awsSqsDlqClient.sendMessage(sqsConfigProperties.dlqName.queueUrl(), message)
+      await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 1 }
+
+      webTestClient.put()
+        .uri("/queue-admin/purge-queue/${sqsConfigProperties.dlqName}")
+        .headers(setAuthorisation(roles = listOf("ROLE_AUDIT_API_QUEUE_ADMIN"), scopes = listOf("write")))
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isOk
+      await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 0 }
+    }
+  }
+}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/integration/queues/RetryDlqTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/integration/queues/RetryDlqTest.kt
@@ -35,10 +35,20 @@ class RetryDlqTest : QueueListenerIntegrationTest() {
     }
 
     @Test
+    fun `should fail if using default rather than custom role`() {
+      webTestClient.put()
+        .uri("/queue-admin/retry-dlq/${sqsConfigProperties.dlqName}")
+        .headers(setAuthorisation(roles = listOf("ROLE_QUEUE_ADMIN")))
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
     fun `should fail it dlq does not exist`() {
       webTestClient.put()
         .uri("/queue-admin/retry-dlq/UNKNOWN_DLQ")
-        .headers(setAuthorisation(roles = listOf("ROLE_QUEUE_ADMIN"), scopes = listOf("write")))
+        .headers(setAuthorisation(roles = listOf("ROLE_AUDIT_API_QUEUE_ADMIN"), scopes = listOf("write")))
         .contentType(MediaType.APPLICATION_JSON)
         .exchange()
         .expectStatus().isNotFound
@@ -63,7 +73,7 @@ class RetryDlqTest : QueueListenerIntegrationTest() {
 
       webTestClient.put()
         .uri("/queue-admin/retry-dlq/${sqsConfigProperties.dlqName}")
-        .headers(setAuthorisation(roles = listOf("ROLE_QUEUE_ADMIN"), scopes = listOf("write")))
+        .headers(setAuthorisation(roles = listOf("ROLE_AUDIT_API_QUEUE_ADMIN"), scopes = listOf("write")))
         .contentType(MediaType.APPLICATION_JSON)
         .exchange()
         .expectStatus().isOk
@@ -76,24 +86,6 @@ class RetryDlqTest : QueueListenerIntegrationTest() {
 
   @Nested
   inner class RetryAllDlqs {
-    @Test
-    fun `should fail if no token`() {
-      webTestClient.put()
-        .uri("/queue-admin/retry-all-dlq")
-        .contentType(MediaType.APPLICATION_JSON)
-        .exchange()
-        .expectStatus().isUnauthorized
-    }
-
-    @Test
-    fun `should fail if wrong role`() {
-      webTestClient.put()
-        .uri("/queue-admin/retry-all-dlqs")
-        .headers(setAuthorisation(roles = listOf("ROLE_WRONG")))
-        .contentType(MediaType.APPLICATION_JSON)
-        .exchange()
-        .expectStatus().isForbidden
-    }
 
     @Test
     fun `should process message from the DLQ`() {
@@ -114,7 +106,7 @@ class RetryDlqTest : QueueListenerIntegrationTest() {
 
       webTestClient.put()
         .uri("/queue-admin/retry-all-dlqs")
-        .headers(setAuthorisation(roles = listOf("ROLE_QUEUE_ADMIN"), scopes = listOf("write")))
+        .headers(setAuthorisation(roles = listOf("ROLE_AUDIT_API_QUEUE_ADMIN"), scopes = listOf("write")))
         .contentType(MediaType.APPLICATION_JSON)
         .exchange()
         .expectStatus().isOk
