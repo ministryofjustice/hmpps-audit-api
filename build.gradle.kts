61b50dd01ba3f7e4e3a9b4ccd17efb70647547fb
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2022-01-27 18:16:51 +0000

    HAAR-92: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.0.2 includes springboot 2.6.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.2"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
 }
@@ -13,22 +13,22 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.3")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.5")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.3")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.3")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.3")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.5")
 
   implementation("org.apache.commons:commons-lang3:3.12.0")
 
-  runtimeOnly("com.h2database:h2:2.0.206")
-  runtimeOnly("org.flywaydb:flyway-core:8.4.0")
+  runtimeOnly("com.h2database:h2:2.1.210")
+  runtimeOnly("org.flywaydb:flyway-core:8.4.3")
   runtimeOnly("org.postgresql:postgresql:42.3.1")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-  testImplementation("org.mockito:mockito-inline:4.2.0")
+  testImplementation("org.mockito:mockito-inline:4.3.1")
 }
 
 java {
