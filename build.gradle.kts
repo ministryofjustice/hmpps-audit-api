cd7871880e861c3ae6b0e6cda1f8f4a04f7d01ad
AlTurner-MOJ <101654848+AlTurner-MOJ@users.noreply.github.com>
2022-03-29 08:27:06 +0100

    HAAR-284: dependencies update, including uk.gov.justice.hmpps.gradle-spring-boot uplift to 4.1.2-beta-2 (#100)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.2-beta-2"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
 }
@@ -13,7 +13,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.6")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.2")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
@@ -22,7 +22,7 @@ dependencies {
   implementation("org.apache.commons:commons-lang3:3.12.0")
 
   runtimeOnly("com.h2database:h2:2.1.210")
-  runtimeOnly("org.flywaydb:flyway-core:8.5.2")
+  runtimeOnly("org.flywaydb:flyway-core:8.5.4")
   runtimeOnly("org.postgresql:postgresql:42.3.3")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
