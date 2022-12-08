11884ddd67b252e0f5a4229a15a8923ce2e8ec45
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-12-08 14:10:48 +0000

    HAAR-1056: ⬆️ update to spring boot plugin 4.7.2-beta (#151)
    * HAAR-1056: ⬆️ update to spring boot plugin 4.7.2-beta
    
    * Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.7.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.7.2-beta-1"
   kotlin("plugin.spring") version "1.7.22"
   kotlin("plugin.jpa") version "1.7.22"
 }
@@ -13,7 +13,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.13")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.2.0")
 
   implementation("org.springdoc:springdoc-openapi-ui:1.6.13")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.13")
