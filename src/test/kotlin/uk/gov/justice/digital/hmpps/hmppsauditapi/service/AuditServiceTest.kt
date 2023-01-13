b423e696a859fc0567e379a5151fad95cd39e338
AlTurner-MOJ <alan.turner@digital.justice.gov.uk>
2023-01-13 11:51:50 +0000

    HAAR-672 - unit test refinement - pairing with Ola

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/service/AuditServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/service/AuditServiceTest.kt
@@ -19,6 +19,7 @@ import uk.gov.justice.digital.hmpps.hmppsauditapi.resource.AuditDto
 import uk.gov.justice.digital.hmpps.hmppsauditapi.services.AuditService
 import java.time.Instant
 import java.util.UUID
+import org.mockito.kotlin.verify
 
 class AuditServiceTest {
   private val telemetryClient: TelemetryClient = mock()
@@ -224,29 +225,6 @@ class AuditServiceTest {
       val listOfAudits = PageImpl(
         listOf(
           AuditEvent(
-            UUID.fromString("64505f1e-c9ca-4e54-8c62-d946359b667f"),
-            "MINIMUM_FIELDS_EVENT",
-            Instant.parse("2021-04-04T17:17:30Z")
-          ),
-          AuditEvent(
-            UUID.fromString("5c5ba3d7-0707-42f1-b9ea-949e22dc17ba"),
-            "COURT_REGISTER_BUILDING_UPDATE",
-            Instant.parse("2021-04-03T10:15:30Z"),
-            "badea6d876c62e2f5264c94c7b50875e",
-            "bobby.beans",
-            "court-register",
-            "{\"courtId\":\"AAAMH1\",\"buildingId\":936,\"building\":{\"id\":936,\"courtId\":\"AAAMH1\",\"buildingName\":\"Main Court Name Changed\"}}"
-          ),
-          AuditEvent(
-            UUID.fromString("e5b4800c-dc4e-45f8-826c-877b1f3ce8de"),
-            "OFFENDER_DELETED",
-            Instant.parse("2021-04-01T15:15:30Z"),
-            "cadea6d876c62e2f5264c94c7b50875e",
-            "bobby.beans",
-            "offender-service",
-            "{\"offenderId\": \"97\"}"
-          ),
-          AuditEvent(
             UUID.fromString("03a1624a-54e7-453e-8c79-816dbe02fd3c"),
             "OFFENDER_DELETED",
             Instant.parse("2020-12-31T08:11:30Z"),
@@ -270,42 +248,23 @@ class AuditServiceTest {
         listOfAudits
       )
 
+      val startDate = Instant.parse("2021-04-04T17:17:30Z")
+      val endDate = Instant.parse("2023-01-03T17:17:30Z")
+      val pageDetails = Pageable.unpaged()
+
       val auditFilterDto = AuditFilterDto(
-        Instant.parse("2021-04-04T17:17:30Z"),
-        Instant.parse("2023-01-03T17:17:30Z"),
+        startDate,
+        endDate,
         "offender-service",
-        "Another Event",
-        "Hola T"
+        "Hola T",
+        "Another Event"
       )
 
-      val audits = auditService.findPage(Pageable.unpaged(), auditFilterDto)
+      val audits = auditService.findPage(pageDetails, auditFilterDto)
+
       assertThat(audits).isEqualTo(
         PageImpl(
           listOf(
-            AuditDto(
-              UUID.fromString("64505f1e-c9ca-4e54-8c62-d946359b667f"),
-              "MINIMUM_FIELDS_EVENT",
-              Instant.parse("2021-04-04T17:17:30Z"),
-              null, null, null, null
-            ),
-            AuditDto(
-              UUID.fromString("5c5ba3d7-0707-42f1-b9ea-949e22dc17ba"),
-              "COURT_REGISTER_BUILDING_UPDATE",
-              Instant.parse("2021-04-03T10:15:30Z"),
-              "badea6d876c62e2f5264c94c7b50875e",
-              "bobby.beans",
-              "court-register",
-              "{\"courtId\":\"AAAMH1\",\"buildingId\":936,\"building\":{\"id\":936,\"courtId\":\"AAAMH1\",\"buildingName\":\"Main Court Name Changed\"}}"
-            ),
-            AuditDto(
-              UUID.fromString("e5b4800c-dc4e-45f8-826c-877b1f3ce8de"),
-              "OFFENDER_DELETED",
-              Instant.parse("2021-04-01T15:15:30Z"),
-              "cadea6d876c62e2f5264c94c7b50875e",
-              "bobby.beans",
-              "offender-service",
-              "{\"offenderId\": \"97\"}"
-            ),
             AuditDto(
               UUID.fromString("03a1624a-54e7-453e-8c79-816dbe02fd3c"),
               "OFFENDER_DELETED",
@@ -318,6 +277,8 @@ class AuditServiceTest {
           )
         )
       )
+
+      verify(auditRepository).findPage(pageDetails, startDate, endDate, "offender-service","Another Event", "Hola T")
     }
   }
 }
