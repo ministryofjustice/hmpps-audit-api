0fe9b4b7dc7074bb30d36f1e5c2d5df9df1c76cf
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2024-01-31 10:52:16 +0000

    AuthUserUuidRepositoryTest

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/jpa/AuthUserUuidRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/jpa/AuthUserUuidRepository.kt
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.hmppsauditapi.jpa.model.AuthUserUuid
 import java.util.UUID
 
-// TODO test
 @Repository
 interface AuthUserUuidRepository : CrudRepository<AuthUserUuid, Long> {
   fun findAllByUserUuid(userUuid: UUID): List<AuthUserUuid>
