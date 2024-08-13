8fb89dbaaf44176b3ba4ae516a51607cc005bf24
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2024-08-13 10:12:03 +0100

    WIP

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/services/AuditS3Client.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/services/AuditS3Client.kt
@@ -1,11 +1,13 @@
 package uk.gov.justice.digital.hmpps.hmppsauditapi.services
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import uk.gov.justice.digital.hmpps.hmppsauditapi.config.trackEvent
 import uk.gov.justice.digital.hmpps.hmppsauditapi.listeners.HMPPSAuditListener
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
@@ -15,6 +17,7 @@ import java.time.ZoneId
 class AuditS3Client(
   private val s3Client: S3Client,
   private val objectMapper: ObjectMapper,
+  private val telemetryClient: TelemetryClient,
   @Value("\${aws.s3.auditBucketName}") private val bucketName: String,
 ) {
 
@@ -29,7 +32,12 @@ class AuditS3Client(
 
     val jsonBytes = auditEventJsonString.toByteArray(StandardCharsets.UTF_8)
 
-    s3Client.putObject(putObjectRequest, RequestBody.fromBytes(jsonBytes))
+    try {
+      s3Client.putObject(putObjectRequest, RequestBody.fromBytes(jsonBytes))
+      telemetryClient.trackEvent("hmpps-audit-mohamad", mapOf("success" to "successful" ))
+    } catch (e: Exception) {
+      telemetryClient.trackEvent("hmpps-audit-mohamad", mapOf("errorMessage" to (e.message ?: "Unknown error")))
+    }
   }
 
   private fun generateFilename(auditEvent: HMPPSAuditListener.AuditEvent, auditEventJsonString: String): String {
