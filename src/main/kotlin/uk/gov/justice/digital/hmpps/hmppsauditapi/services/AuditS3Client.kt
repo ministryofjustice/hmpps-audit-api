18972f006b33f8471f1dd1f0f0dd042fa2e99b8a
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2024-09-17 15:50:02 +0100

    cleanup

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/services/AuditS3Client.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/services/AuditS3Client.kt
@@ -13,12 +13,8 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.GetObjectRequest
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
-import uk.gov.justice.digital.hmpps.hmppsauditapi.config.trackEvent
 import uk.gov.justice.digital.hmpps.hmppsauditapi.listeners.HMPPSAuditListener
-import java.io.PrintWriter
-import java.io.StringWriter
 import java.nio.file.Files
 import java.security.MessageDigest
 import java.time.ZoneId
@@ -27,34 +23,22 @@ import java.util.Base64
 @Service
 class AuditS3Client(
   private val s3Client: S3Client,
-  private val telemetryClient: TelemetryClient,
   @Value("\${aws.s3.auditBucketName}") private val bucketName: String,
 ) {
 
   fun save(auditEvent: HMPPSAuditListener.AuditEvent) {
-    try {
-      val fileName = generateFilename(auditEvent)
-      val parquetBytes = convertToParquetBytes(auditEvent, fileName)
-      val md5Digest = MessageDigest.getInstance("MD5").digest(parquetBytes)
-      val md5Base64 = Base64.getEncoder().encodeToString(md5Digest)
+    val fileName = generateFilename(auditEvent)
+    val parquetBytes = convertToParquetBytes(auditEvent, fileName)
+    val md5Digest = MessageDigest.getInstance("MD5").digest(parquetBytes)
+    val md5Base64 = Base64.getEncoder().encodeToString(md5Digest)
 
-      val putObjectRequest = PutObjectRequest.builder()
-        .bucket(bucketName)
-        .key(fileName)
-        .contentMD5(md5Base64)
-        .build()
+    val putObjectRequest = PutObjectRequest.builder()
+      .bucket(bucketName)
+      .key(fileName)
+      .contentMD5(md5Base64)
+      .build()
 
-      s3Client.putObject(putObjectRequest, RequestBody.fromBytes(parquetBytes))
-
-      readParquetFileFromS3(fileName)
-    } catch (e: Exception) {
-      telemetryClient.trackEvent("mohamad-test", mapOf(Pair("error", e.message ?: "unknown error")))
-
-      val stringWriter = StringWriter()
-      e.printStackTrace(PrintWriter(stringWriter))
-      val stackTraceAsString = stringWriter.toString()
-      telemetryClient.trackEvent("mohamad-test", mapOf("cause" to stackTraceAsString))
-    }
+    s3Client.putObject(putObjectRequest, RequestBody.fromBytes(parquetBytes))
   }
 
   private fun generateFilename(auditEvent: HMPPSAuditListener.AuditEvent): String {
@@ -89,22 +73,5 @@ class AuditS3Client(
 
     val parquetBytes: ByteArray = Files.readAllBytes(java.nio.file.Path.of(System.getProperty("java.io.tmpdir"), "parquet-${auditEvent.id}.parquet"))
     return parquetBytes
-  }
-
-  private fun readParquetFileFromS3(fileName: String) {
-    val tempFile = Files.createTempFile("test-parquet", ".parquet").toFile()
-    val getObjectRequest = GetObjectRequest.builder()
-      .bucket(bucketName)
-      .key(fileName)
-      .build()
-    s3Client.getObject(getObjectRequest, tempFile.toPath())
-    val reader = AvroParquetReader.builder<GenericRecord>(Path(tempFile.toURI())).build()
-
-    var record: GenericRecord?
-    while (true) {
-      record = reader.read()
-      telemetryClient.trackEvent("mohamad-test", mapOf("record" to record.toString()))
-    }
-    reader.close()
   }
 }
