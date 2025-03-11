855a06baddd44d929d75a6e7eb646d78bcfa8778
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-03-11 16:47:05 +0000

    ktlint

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/integration/queues/RetryDlqTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/integration/queues/RetryDlqTest.kt
@@ -16,50 +16,50 @@ import uk.gov.justice.digital.hmpps.hmppsauditapi.resource.QueueListenerIntegrat
 @TestPropertySource(properties = ["hmpps.repository.saveToS3Bucket=false"])
 class RetryDlqTest : QueueListenerIntegrationTest() {
 
-    @Nested
-    inner class RetryDlq {
-        @Test
-        fun `should fail if no token`() {
-            webTestClient.put()
-                .uri("/queue-admin/retry-dlq/${auditQueueConfig.dlqName}")
-                .contentType(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isUnauthorized
-        }
+  @Nested
+  inner class RetryDlq {
+    @Test
+    fun `should fail if no token`() {
+      webTestClient.put()
+        .uri("/queue-admin/retry-dlq/${auditQueueConfig.dlqName}")
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isUnauthorized
+    }
 
-        @Test
-        fun `should fail if wrong role`() {
-            webTestClient.put()
-                .uri("/queue-admin/retry-dlq/${auditQueueConfig.dlqName}")
-                .headers(setAuthorisation(roles = listOf("ROLE_WRONG")))
-                .contentType(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isForbidden
-        }
+    @Test
+    fun `should fail if wrong role`() {
+      webTestClient.put()
+        .uri("/queue-admin/retry-dlq/${auditQueueConfig.dlqName}")
+        .headers(setAuthorisation(roles = listOf("ROLE_WRONG")))
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isForbidden
+    }
 
-        @Test
-        fun `should fail if using default rather than custom role`() {
-            webTestClient.put()
-                .uri("/queue-admin/retry-dlq/${auditQueueConfig.dlqName}")
-                .headers(setAuthorisation(roles = listOf("ROLE_QUEUE_ADMIN")))
-                .contentType(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isForbidden
-        }
+    @Test
+    fun `should fail if using default rather than custom role`() {
+      webTestClient.put()
+        .uri("/queue-admin/retry-dlq/${auditQueueConfig.dlqName}")
+        .headers(setAuthorisation(roles = listOf("ROLE_QUEUE_ADMIN")))
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isForbidden
+    }
 
-        @Test
-        fun `should fail it dlq does not exist`() {
-            webTestClient.put()
-                .uri("/queue-admin/retry-dlq/UNKNOWN_DLQ")
-                .headers(setAuthorisation(roles = listOf("ROLE_AUDIT_API_QUEUE_ADMIN"), scopes = listOf("write")))
-                .contentType(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isNotFound
-        }
+    @Test
+    fun `should fail it dlq does not exist`() {
+      webTestClient.put()
+        .uri("/queue-admin/retry-dlq/UNKNOWN_DLQ")
+        .headers(setAuthorisation(roles = listOf("ROLE_AUDIT_API_QUEUE_ADMIN"), scopes = listOf("write")))
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isNotFound
+    }
 
-        @Test
-        fun `should process message from the DLQ`() {
-            val message = """
+    @Test
+    fun `should process message from the DLQ`() {
+      val message = """
     {
       "what": "OFFENDER_DELETED",
       "when": "2021-01-25T12:30:00Z",
@@ -69,32 +69,32 @@ class RetryDlqTest : QueueListenerIntegrationTest() {
       "details": "{ \"offenderId\": \"99\"}"
     }
   """
-            await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
-            await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 0 }
-            awsSqsClient.sendMessage(
-                SendMessageRequest.builder().queueUrl(awsSqsDlqUrl).messageBody(message).build(),
-            )
-            await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 1 }
+      await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+      await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 0 }
+      awsSqsClient.sendMessage(
+        SendMessageRequest.builder().queueUrl(awsSqsDlqUrl).messageBody(message).build(),
+      )
+      await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 1 }
 
-            webTestClient.put()
-                .uri("/queue-admin/retry-dlq/${auditQueueConfig.dlqName}")
-                .headers(setAuthorisation(roles = listOf("ROLE_AUDIT_API_QUEUE_ADMIN"), scopes = listOf("write")))
-                .contentType(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk
-            await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 0 }
-            await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+      webTestClient.put()
+        .uri("/queue-admin/retry-dlq/${auditQueueConfig.dlqName}")
+        .headers(setAuthorisation(roles = listOf("ROLE_AUDIT_API_QUEUE_ADMIN"), scopes = listOf("write")))
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isOk
+      await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 0 }
+      await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
 
-            verify(auditRepository).save(any<AuditEvent>())
-        }
+      verify(auditRepository).save(any<AuditEvent>())
     }
+  }
 
-    @Nested
-    inner class RetryAllDlqs {
+  @Nested
+  inner class RetryAllDlqs {
 
-        @Test
-        fun `should process message from the DLQ`() {
-            val message = """
+    @Test
+    fun `should process message from the DLQ`() {
+      val message = """
     {
       "what": "OFFENDER_DELETED",
       "when": "2021-01-25T12:30:00Z",
@@ -104,22 +104,22 @@ class RetryDlqTest : QueueListenerIntegrationTest() {
       "details": "{ \"offenderId\": \"99\"}"
     }
   """
-            await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
-            await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 0 }
-            awsSqsDlqClient.sendMessage(
-                SendMessageRequest.builder().queueUrl(awsSqsDlqUrl).messageBody(message).build(),
-            )
-            await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 1 }
+      await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+      await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 0 }
+      awsSqsDlqClient.sendMessage(
+        SendMessageRequest.builder().queueUrl(awsSqsDlqUrl).messageBody(message).build(),
+      )
+      await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 1 }
 
-            webTestClient.put()
-                .uri("/queue-admin/retry-all-dlqs")
-                .contentType(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk
-            await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 0 }
-            await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
+      webTestClient.put()
+        .uri("/queue-admin/retry-all-dlqs")
+        .contentType(MediaType.APPLICATION_JSON)
+        .exchange()
+        .expectStatus().isOk
+      await untilCallTo { getNumberOfMessagesCurrentlyOnDlq() } matches { it == 0 }
+      await untilCallTo { getNumberOfMessagesCurrentlyOnQueue() } matches { it == 0 }
 
-            verify(auditRepository).save(any<AuditEvent>())
-        }
+      verify(auditRepository).save(any<AuditEvent>())
     }
+  }
 }
