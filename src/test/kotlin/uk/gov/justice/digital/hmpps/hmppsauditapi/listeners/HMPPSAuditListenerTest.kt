6a4231ab72a5be5e5077cfcd503d2dabb60ced84
ma226860 <142583965+ma226860@users.noreply.github.com>
2025-03-20 14:10:20 +0000

    HAAR-3748 Bugfix on non-stringified JSON details (#254)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/listeners/HMPPSAuditListenerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/listeners/HMPPSAuditListenerTest.kt
@@ -12,14 +12,14 @@ import uk.gov.justice.digital.hmpps.hmppsauditapi.resource.QueueListenerIntegrat
 internal class HMPPSAuditListenerTest : QueueListenerIntegrationTest() {
 
   @Test
-  internal fun `will call service for an audit event`() {
+  internal fun `will call service for an audit event with JSON details`() {
     val message = """
     {
       "what": "OFFENDER_DELETED",
       "when": "2021-01-25T12:30:00Z",
       "who": "bobby.beans",
       "service": "offender-service",
-      "details": "{ \"offenderId\": \"99\"}"
+      "details": "{ \"offenderId\": \"99\" }"
     }
   """
     listener.onAuditEvent(message)
@@ -28,11 +28,38 @@ internal class HMPPSAuditListenerTest : QueueListenerIntegrationTest() {
 
     verify(auditService).audit(
       check {
-        assertThat(it.details).isEqualTo("{ \"offenderId\": \"99\"}")
+        assertThat(it.details).isEqualTo("{ \"offenderId\": \"99\" }")
         assertThat(it.`when`).isEqualTo("2021-01-25T12:30:00Z")
         assertThat(it.what).isEqualTo("OFFENDER_DELETED")
         assertThat(it.who).isEqualTo("bobby.beans")
         assertThat(it.service).isEqualTo("offender-service")
+      },
+    )
+  }
+
+  @Test
+  internal fun `will call service for an audit event with regular string details`() {
+    val message = """
+    {
+      "what": "USER_LOGIN",
+      "when": "2021-01-25T12:35:00Z",
+      "who": "alice.jones",
+      "service": "auth-service",
+      "details": "non-json-stringified details"
+    }
+  """
+
+    doNothing().whenever(auditService).audit(any())
+
+    listener.onAuditEvent(message)
+
+    verify(auditService).audit(
+      check {
+        assertThat(it.details).isEqualTo("{\"details\":\"non-json-stringified details\"}")
+        assertThat(it.`when`).isEqualTo("2021-01-25T12:35:00Z")
+        assertThat(it.what).isEqualTo("USER_LOGIN")
+        assertThat(it.who).isEqualTo("alice.jones")
+        assertThat(it.service).isEqualTo("auth-service")
       },
     )
   }
