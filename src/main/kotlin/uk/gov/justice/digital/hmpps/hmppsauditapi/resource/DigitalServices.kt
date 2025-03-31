29818f55a3516ddbb130c641ffccd2cb3f58453b
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-03-31 14:49:37 +0100

    DigitalServices

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/resource/DigitalServices.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/resource/DigitalServices.kt
@@ -1,16 +1,20 @@
 package uk.gov.justice.digital.hmpps.hmppsauditapi.resource
 
+import com.microsoft.applicationinsights.TelemetryClient
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import jakarta.validation.Valid
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsauditapi.config.trackEvent
 import uk.gov.justice.digital.hmpps.hmppsauditapi.model.DigitalServicesQueryRequest
 import uk.gov.justice.digital.hmpps.hmppsauditapi.model.DigitalServicesQueryResponse
 import uk.gov.justice.digital.hmpps.hmppsauditapi.resource.swagger.StandardApiResponses
@@ -23,6 +27,7 @@ import java.util.UUID
 class DigitalServices(
   private val auditService: AuditService,
   private val auditQueueService: AuditQueueService,
+  private val telemetryClient: TelemetryClient,
 ) {
 
   @PreAuthorize("hasRole('ROLE_AUDIT') and hasAuthority('SCOPE_read')")
@@ -40,7 +45,18 @@ class DigitalServices(
       AuditType.AUDIT_GET_BY_USER.name,
       auditFilterDto,
     )
-    return auditService.triggerQuery(auditFilterDto, listOf("hmpps-manage-users"))
+    val authorisedServices = getAuthorisedServices()
+    telemetryClient.trackEvent("mohamad-test", mapOf(Pair("authorisedServices", authorisedServices.toString()))) // TODO remove
+    return auditService.triggerQuery(auditFilterDto, authorisedServices)
+  }
+
+  fun getAuthorisedServices(): List<String> {
+    val authentication = SecurityContextHolder.getContext().authentication
+    return authentication?.authorities
+      ?.map(GrantedAuthority::getAuthority)
+      ?.filter { it.startsWith("ROLE_QUERY_AUDIT__") }
+      ?.map { it.removePrefix("ROLE_QUERY_AUDIT__").lowercase().replace('_', '-') }
+      ?: emptyList()
   }
 
   @PreAuthorize("hasRole('ROLE_AUDIT') and hasAuthority('SCOPE_read')")
