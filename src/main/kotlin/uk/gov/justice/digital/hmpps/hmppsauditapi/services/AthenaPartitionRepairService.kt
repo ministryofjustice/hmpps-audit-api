19a9842f850819c592dd5b15e77b38dc344c091a
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-04-29 14:48:22 +0100

    AthenaPartitionRepairService

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/services/AthenaPartitionRepairService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/services/AthenaPartitionRepairService.kt
@@ -1,17 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppsauditapi.services
 
-import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import software.amazon.awssdk.services.athena.AthenaClient
 import software.amazon.awssdk.services.athena.model.StartQueryExecutionRequest
-import uk.gov.justice.digital.hmpps.hmppsauditapi.config.trackEvent
 
 @Component
 class AthenaPartitionRepairService(
   private val athenaClient: AthenaClient,
-  private val telemetryClient: TelemetryClient,
   @Value("\${aws.athena.database}") private val databaseName: String,
   @Value("\${aws.athena.workgroup}") private val workGroup: String,
   @Value("\${aws.athena.outputLocation}") private val outputLocation: String,
@@ -19,20 +16,13 @@ class AthenaPartitionRepairService(
 
   @Scheduled(cron = "0 0 * * * *")
   fun repairPartitions() {
-    try {
-      val repairQuery = "MSCK REPAIR TABLE $databaseName.audit_event;"
-
-      val request = StartQueryExecutionRequest.builder()
-        .queryString(repairQuery)
-        .queryExecutionContext { it.database(databaseName) }
-        .workGroup(workGroup)
-        .resultConfiguration { it.outputLocation(outputLocation) }
-        .build()
-
-      val response = athenaClient.startQueryExecution(request)
-      telemetryClient.trackEvent("partition", mapOf("partition query execution ID" to response.queryExecutionId()))
-    } catch (ex: Exception) {
-      telemetryClient.trackEvent("partition-error", mapOf("error" to ex.message.toString()))
-    }
+    val repairQuery = "MSCK REPAIR TABLE $databaseName.audit_event;"
+    val request = StartQueryExecutionRequest.builder()
+      .queryString(repairQuery)
+      .queryExecutionContext { it.database(databaseName) }
+      .workGroup(workGroup)
+      .resultConfiguration { it.outputLocation(outputLocation) }
+      .build()
+    athenaClient.startQueryExecution(request)
   }
 }
