02524582e5c5b8911895696627a54316e6aabcf9
ma226860 <142583965+ma226860@users.noreply.github.com>
2025-05-29 10:41:55 +0100

    Security fixes (#269)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ configurations {
   all {
     resolutionStrategy {
       force("org.apache.commons:commons-configuration2:2.11.0")
+      force("commons-beanutils:commons-beanutils:1.11.0")
     }
   }
 }
@@ -31,8 +32,8 @@ dependencies {
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
 
   implementation("org.apache.commons:commons-lang3:3.17.0")
-  implementation("software.amazon.awssdk:s3:2.31.49")
-  implementation("software.amazon.awssdk:athena:2.31.49")
+  implementation("software.amazon.awssdk:s3:2.31.52")
+  implementation("software.amazon.awssdk:athena:2.31.52")
   implementation("org.apache.parquet:parquet-avro:1.15.2")
   implementation("org.apache.avro:avro:1.12.0")
   implementation("org.apache.hadoop:hadoop-client:3.4.1") {
@@ -46,12 +47,14 @@ dependencies {
     exclude(group = "org.eclipse.jetty", module = "jetty-webapp")
     exclude(group = "org.eclipse.jetty.websocket", module = "websocket-common")
     exclude(group = "org.eclipse.jetty.websocket", module = "websocket-client")
+    exclude(group = "commons-beanutils", module = "commons-beanutils")
   }
+  implementation("commons-beanutils:commons-beanutils:1.11.0")
 
   runtimeOnly("com.h2database:h2:2.3.232")
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
-  runtimeOnly("org.postgresql:postgresql:42.7.5")
+  runtimeOnly("org.postgresql:postgresql:42.7.6")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
