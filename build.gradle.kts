5b8751a05d25d7f8c0877b6ab1837b9d45e2b6c4
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-07-04 11:52:43 +0100

    reverted all gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
-  kotlin("plugin.spring") version "2.2.0"
-  kotlin("plugin.jpa") version "2.2.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0"
+  kotlin("plugin.spring") version "2.1.21"
+  kotlin("plugin.jpa") version "2.1.21"
 }
 
 configurations {
@@ -11,7 +11,6 @@ configurations {
   all {
     resolutionStrategy {
       force("org.apache.commons:commons-configuration2:2.11.0")
-      force("commons-beanutils:commons-beanutils:1.11.0")
     }
   }
 }
@@ -28,14 +27,15 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.6")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.5")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
 
   implementation("org.apache.commons:commons-lang3:3.17.0")
   implementation("software.amazon.awssdk:s3:2.31.49")
   implementation("software.amazon.awssdk:athena:2.31.49")
   implementation("org.apache.parquet:parquet-avro:1.15.2")
   implementation("org.apache.avro:avro:1.12.0")
+  runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
   implementation("org.apache.hadoop:hadoop-client:3.4.1") {
     exclude(group = "com.google.protobuf", module = "protobuf-java")
     exclude(group = "org.apache.hadoop.thirdparty", module = "hadoop-shaded-protobuf_3_25")
@@ -47,14 +47,12 @@ dependencies {
     exclude(group = "org.eclipse.jetty", module = "jetty-webapp")
     exclude(group = "org.eclipse.jetty.websocket", module = "websocket-common")
     exclude(group = "org.eclipse.jetty.websocket", module = "websocket-client")
-    exclude(group = "commons-beanutils", module = "commons-beanutils")
   }
-  implementation("commons-beanutils:commons-beanutils:1.11.0")
 
   runtimeOnly("com.h2database:h2:2.3.232")
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
-  runtimeOnly("org.postgresql:postgresql:42.7.7")
+  runtimeOnly("org.postgresql:postgresql:42.7.5")
 
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
