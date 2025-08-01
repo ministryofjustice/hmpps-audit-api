311d92b80890f873e9c07278ae8e6bf7dc16c52f
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-08-01 15:57:12 +0100

    ktlintformat

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/resource/AuditIntegrationTestController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/resource/AuditIntegrationTestController.kt
@@ -1,17 +1,12 @@
 package uk.gov.justice.digital.hmpps.hmppsauditapi.resource
 
 import com.microsoft.applicationinsights.TelemetryClient
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsauditapi.config.trackEvent
 import uk.gov.justice.digital.hmpps.hmppsauditapi.listeners.HMPPSAuditListener.AuditEvent
