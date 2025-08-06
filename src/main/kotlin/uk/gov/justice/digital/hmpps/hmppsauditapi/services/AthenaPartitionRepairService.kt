afe8980699a74d2fbdf78f1d39615161b2882736
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2025-08-06 07:48:17 +0100

    final changes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/services/AthenaPartitionRepairService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsauditapi/services/AthenaPartitionRepairService.kt
@@ -5,12 +5,9 @@ import org.springframework.stereotype.Component
 import software.amazon.awssdk.services.athena.AthenaClient
 import software.amazon.awssdk.services.athena.model.QueryExecutionState.QUEUED
 import software.amazon.awssdk.services.athena.model.StartQueryExecutionRequest
-import uk.gov.justice.digital.hmpps.hmppsauditapi.config.AthenaProperties
 import uk.gov.justice.digital.hmpps.hmppsauditapi.config.AthenaPropertiesFactory
 import uk.gov.justice.digital.hmpps.hmppsauditapi.listeners.model.AuditEventType
 import uk.gov.justice.digital.hmpps.hmppsauditapi.model.AthenaQueryResponse
-import java.time.LocalDate
-import java.time.ZoneId
 import java.util.UUID
 
 @Component
@@ -21,34 +18,12 @@ class AthenaPartitionRepairService(
 
   fun triggerRepairPartitions(auditEventType: AuditEventType): AthenaQueryResponse {
     val athenaProperties = athenaPropertiesFactory.getProperties(auditEventType)
-    val repairQuery = "MSCK REPAIR TABLE ${athenaProperties.databaseName}.${athenaProperties.tableName};"
-    return startAthenaQuery(repairQuery, athenaProperties)
-  }
-
-  fun triggerRepairForUserToday(auditEventType: AuditEventType, who: String): AthenaQueryResponse { // TODO test
-    val today = LocalDate.now(ZoneId.systemDefault())
-    val athenaProperties = athenaPropertiesFactory.getProperties(auditEventType)
-
-    val year = today.year
-    val month = today.monthValue
-    val day = today.dayOfMonth
-
-    val partitionS3Path = "s3://${athenaProperties.s3BucketName}/year=$year/month=$month/day=$day/user=$who/"
-
-    val alterQuery = """
-        ALTER TABLE ${athenaProperties.databaseName}.${athenaProperties.tableName}
-        ADD IF NOT EXISTS PARTITION (year=$year, month=$month, day=$day, user='$who')
-        LOCATION '$partitionS3Path'
-    """.trimIndent()
-
-    return startAthenaQuery(alterQuery, athenaProperties)
-  }
-
-  // TODO move into athena client?
-  private fun startAthenaQuery(query: String, athenaProperties: AthenaProperties): AthenaQueryResponse {
+    val databaseName = athenaProperties.databaseName
+    val tableName = athenaProperties.tableName
+    val repairQuery = "MSCK REPAIR TABLE $databaseName.$tableName;"
     val request = StartQueryExecutionRequest.builder()
-      .queryString(query)
-      .queryExecutionContext { it.database(athenaProperties.databaseName) }
+      .queryString(repairQuery)
+      .queryExecutionContext { it.database(databaseName) }
       .workGroup(athenaProperties.workGroupName)
       .resultConfiguration { it.outputLocation(athenaProperties.outputLocation) }
       .build()
