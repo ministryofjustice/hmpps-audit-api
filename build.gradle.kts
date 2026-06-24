071ab6f4af400f7b54f6e3365fd35b7e85d0d6ac
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2026-06-24 13:04:08 +0100

    HAAR-5647: ⬆️ update to spring-boot-plugin-10.5.2 (#373)
    * HAAR-5647: ⬆️ update to spring-boot-plugin-10.5.2
    
    * HAAR-5647: ⬆️ pin springdoc-openapi-starter-webmvc-ui to 3.0.2
    
    * HAAR-5647: ⬆️ pin springdoc-openapi-starter-webmvc-ui to 3.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.3.1"
-  kotlin("plugin.spring") version "2.3.21"
-  kotlin("plugin.jpa") version "2.3.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.5.2"
+  kotlin("plugin.spring") version "2.4.0"
+  kotlin("plugin.jpa") version "2.4.0"
 }
 
 configurations {
@@ -28,15 +28,17 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.3.2")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.4.0")
+  // Temporarily pin spring doc at 3.0.2 whilst waiting for 3.0.4 upgrade
+  val springDocVersion = "3.0.2"
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocVersion")
   constraints {
     implementation("org.webjars:swagger-ui:5.32.2")
   }
 
   implementation("org.apache.commons:commons-lang3:3.20.0")
-  implementation("software.amazon.awssdk:s3:2.44.4")
-  implementation("software.amazon.awssdk:athena:2.44.4")
+  implementation("software.amazon.awssdk:s3:2.46.17")
+  implementation("software.amazon.awssdk:athena:2.46.17")
   implementation("org.apache.parquet:parquet-avro:1.17.1")
   implementation("org.apache.avro:avro:1.12.1")
   implementation("org.apache.hadoop:hadoop-client:3.5.0") {
@@ -60,7 +62,7 @@ dependencies {
 
   implementation("commons-beanutils:commons-beanutils:1.11.0")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
-  implementation("org.glassfish.jaxb:jaxb-runtime:4.0.7")
+  implementation("org.glassfish.jaxb:jaxb-runtime:4.0.9")
 
   runtimeOnly("com.h2database:h2:2.4.240")
   runtimeOnly("org.flywaydb:flyway-core")
